a4243e56db57456019fefead764e45446cf0b3f6
chaithanya628 <chaithuchaithanya0677@gmail.com>
2022-12-19 19:06:11 +0530

    1stcommit

---
+++ Firstproject/Firstapp/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from django.http import HttpResponse;
+
+
+def hello(request):
+	print("hello/ url is requested & hello() is executed");
+	ss='''
+	<html>
+		<head>
+			<title>Hello Webpage</title>
+			<style>
+				h1{
+					color:Blue;
+					width:90%;
+				}
+				h2{
+					color:Green;
+					width:80%;
+				}
+				h3{
+					color:Red;
+					width:70%;
+				}
+				h1,h2,h3{
+					background-color:lightblue;
+					border:2px Solid Brown;
+				}
+			</style>
+		</head>
+		<body>
+			<center>
+				<h1>Hello User..!!!</h1>
+				<hr color='brown' width='80%'/>
+				<h2>Welcome to Django-App</h2>
+				<hr color='brown' width='60%'/>
+				<h3>Have a nice day...</h3>
+				<hr color='brown' width='40%'/>
+			</center>
+		</body>
+	</html>
+	''';
+	return HttpResponse(ss);
+
+
+import time;	
+def senddatetime(request):
+	print("dtime/ url is requested & senddatetime() is executed");
+	ct = time.ctime()
+	print("Client Request Date & time on server :: ",ct);
+	ss='''
+	<html>
+		<head>
+			<title>Date-time Webpage</title>
+			<style>
+				h1{
+					color:Blue;
+					width:90%;
+				}
+				h2{
+					color:Green;
+					width:80%;
+				}
+				h3{
+					color:Red;
+					width:70%;
+				}
+				h1,h2,h3{
+					background-color:lightgreen;
+					border:2px Solid Brown;
+				}
+			</style>
+		</head>
+		<body>
+			<center>
+				<h1>Welcome to DJango-User..!!!</h1>
+				<hr color='brown' width='80%'/>
+				<h2>Server-Date & Time :: </h2>
+				<hr color='brown' width='70%'/>
+				<h3>''',ct,'''</h3>
+				<hr color='brown' width='60%'/>
+			</center>
+		</body>
+	</html>
+	''';
+	return HttpResponse(ss);
